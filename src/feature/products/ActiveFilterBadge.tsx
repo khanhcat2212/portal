e89057337ef8df3721a228301273129c4c71e726
teq-khanhcat <khanhcat@teqnological.asia>
2025-11-06 10:33:23 +0700

    feat: develop products page

---
+++ src/feature/products/ActiveFilterBadge.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { X } from "lucide-react";
+
+interface Props {
+  filter: { field: string; value: string };
+  onRemove: () => void;
+}
+
+const ActiveFilterBadge: React.FC<Props> = ({ filter, onRemove }) => {
+  return (
+    <div className="w-fit rounded-sm bg-grey-300 p-2 text-[12px] text-grey-800 flex items-center justify-between gap-4">
+      {`${filter.field === "status" ? "Tình trạng sản phẩm" : "Ngày tạo"}: ${filter.value}`}
+      <div
+        onClick={onRemove}
+        className="w-5 h-5 rounded-md bg-grey-200 flex items-center justify-center hover:scale-[1.2] transition-all cursor-pointer"
+      >
+        <X className="text-grey-500" size={12} />
+      </div>
+    </div>
+  );
+};
+
+export default ActiveFilterBadge;
--- src/pages/admin/products/index.tsx
+++ src/pages/admin/products/index.tsx
@@ -1,36 +1,111 @@
 import Button from "@src/components/button/Button";
 import Input from "@src/components/input/Input";
+import Pagination from "@src/components/pagination/Pagination";
+import { productFilterOptions } from "@src/constants/productFilterOptions";
 import { products } from "@src/constants/products";
+import ActiveFilterBadge from "@src/feature/products/ActiveFilterBadge";
+import FilterPopup from "@src/feature/products/FIlterPopup";
 import AdminLayout from "@src/layout/adminLayout";
-import { ChevronDown, ChevronLeft, ChevronRight, Filter, Plus } from "lucide-react";
-import React, { useMemo, useState } from "react";
+import { isWithinPeriod, normalizeString } from "@src/utils/parseData";
+import { ChevronDown, Filter, Plus } from "lucide-react";
+import React, { useEffect, useMemo, useState } from "react";
+
+type ProductField = "status" | "createdAt";
+
+interface Filter {
+  field: ProductField;
+  value: string;
+}
 
 const Products: React.FC = () => {
   const [keyword, setKeyword] = useState("");
+  const [isOpenFilter, setIsOpenFilter] = useState(false);
+  const [activeFilters, setActiveFilters] = useState<Filter[]>([]);
+  const [selectedField, setSelectedField] = useState<ProductField>("status");
+  const [selectedValue, setSelectedValue] = useState("");
+  const filterPopupRef = React.useRef<HTMLDivElement>(null);
+
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 3;
 
-  const filteredProducts = useMemo(() => {
-    const kw = keyword.trim().toLowerCase();
-    if (!kw) return products;
-    const normalize = (str: string) =>
-      str
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .toLowerCase();
+  const addFilter = () => {
+    if (!selectedValue) return;
 
-    return products.filter((o) => {
-      const id = normalize(o.id);
-      const name = normalize(o.name);
-      const status = normalize(o.status);
-      const k = normalize(keyword);
-      return id.includes(k) || name.includes(k) || status.includes(k);
-    });
-  }, [keyword]);
+    const newFilter = { field: selectedField, value: selectedValue };
+
+    if (
+      activeFilters.some(
+        (f) => f.field === newFilter.field && f.value === newFilter.value
+      )
+    )
+      return;
+
+    setActiveFilters([...activeFilters, newFilter]);
+    setIsOpenFilter(false);
+    setSelectedValue("");
+  };
+
+  const removeFilter = (index: number) => {
+    setActiveFilters(activeFilters.filter((_, i) => i !== index));
+  };
+
+  const filteredProducts = useMemo(() => {
+    let result = products;
+
+    if (keyword.trim()) {
+      const kw = normalizeString(keyword);
+      result = result.filter(
+        (p) =>
+          normalizeString(p.id).includes(kw) ||
+          normalizeString(p.name).includes(kw) ||
+          normalizeString(p.status).includes(kw)
+      );
+    }
+
+    if (activeFilters.length > 0) {
+      result = result.filter((product) =>
+        activeFilters.every((filter) => {
+          if (filter.field === "createdAt") {
+            return isWithinPeriod(product.createdAt, filter.value);
+          }
+          return product[filter.field] === filter.value;
+        })
+      );
+    }
+
+    return result;
+  }, [keyword, activeFilters]);
+
+  useEffect(() => setCurrentPage(1), [keyword]);
+
+  //   React.useEffect(() => {
+  //     const firstValue = productFilterOptions[selectedField][0];
+  //     setSelectedValue(firstValue);
+  //   }, [selectedField]);
 
   React.useEffect(() => {
-    setCurrentPage(1);
-  }, [keyword]);
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        filterPopupRef.current &&
+        !filterPopupRef.current.contains(event.target as Node)
+      ) {
+        setIsOpenFilter(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isOpenFilter) {
+      const firstValue = productFilterOptions[selectedField][0];
+      setSelectedValue(firstValue);
+    }
+  }, [isOpenFilter, selectedField]);
 
   const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
   const pagesPerBlock = 5;
@@ -56,7 +131,7 @@ const Products: React.FC = () => {
   }
   return (
     <AdminLayout>
-      <div className="w-full h-[468px] bg-white rounded-sm py-4 px-5">
+      <div className="w-full h-[468px] bg-white rounded-sm py-6 px-5 flex flex-col justify-between">
         <div className="flex items-center justify-between gap-2 mb-4">
           <div className="flex-1">
             <Input
@@ -67,12 +142,25 @@ const Products: React.FC = () => {
               onChange={(e) => setKeyword(e.target.value)}
             />
           </div>
-          <div className="bg-grey-300 rounded-md flex items-center justify-center gap-2 w-45 h-10">
+          <div
+            onClick={() => setIsOpenFilter((prev) => !prev)}
+            className="relative cursor-pointer bg-grey-300 rounded-md flex items-center justify-center gap-2 w-45 h-10"
+          >
             <Filter size={14} />
             <span className="text-[.8125rem] font-semibold">
               Thêm điều kiện lọc
             </span>
             <ChevronDown size={14} />
+
+            {isOpenFilter && (
+              <FilterPopup
+                selectedField={selectedField}
+                selectedValue={selectedValue}
+                setSelectedField={setSelectedField}
+                setSelectedValue={setSelectedValue}
+                addFilter={addFilter}
+              />
+            )}
           </div>
 
           <Button variant="primary" size="sm" className="w-33.5">
@@ -83,19 +171,31 @@ const Products: React.FC = () => {
           </Button>
         </div>
 
+        {activeFilters.length > 0 && (
+          <div className="w-full border-t border-b border-grey-200 py-4 flex items-center gap-4">
+            {activeFilters.map((f, idx) => (
+              <ActiveFilterBadge
+                key={idx}
+                filter={f}
+                onRemove={() => removeFilter(idx)}
+              />
+            ))}
+          </div>
+        )}
+
         {/* Table */}
-        <div className="rounded-lg">
+        <div className="rounded-lg h-100">
           <table className="w-full text-left text-[14px] text-black-300">
             <thead>
               <tr className="">
-                <th className="p-4 w-10">
-                  <input type="checkbox" />
+                <th className="px-4 py-3 w-10">
+                  <input type="checkbox" className="w-4 h-4 rounded-lg" />
                 </th>
-                <th className="p-4 font-semibold">Mã sản phẩm / SKU</th>
-                <th className="p-4 font-semibold">Tên sản phẩm</th>
-                <th className="p-4 font-semibold">Tình trạng</th>
-                <th className="p-4 font-semibold">Số lượng tồn kho</th>
-                <th className="p-4 font-semibold">Ngày tạo</th>
+                <th className="px-4 py-3 font-semibold">Mã sản phẩm / SKU</th>
+                <th className="px-4 py-3 font-semibold">Tên sản phẩm</th>
+                <th className="px-4 py-3 font-semibold">Tình trạng</th>
+                <th className="px-4 py-3 font-semibold">Số lượng tồn kho</th>
+                <th className="px-4 py-3 font-semibold">Ngày tạo</th>
               </tr>
             </thead>
 
@@ -105,18 +205,20 @@ const Products: React.FC = () => {
                   key={product.id}
                   className="border-t border-grey-200 hover:bg-slate-50"
                 >
-                  <td className="p-4">
-                    <input type="checkbox" />
+                  <td className="px-4 py-5">
+                    <input type="checkbox" className="w-4 h-4 rounded-lg" />
                   </td>
-                  <td className="p-4">{product.id}</td>
-                  <td className="p-4">{product.name}</td>
-                  <td className="p-4">
-                    <div className={`px-2 py-1 w-fit rounded-md text-white text-[12px] ${product.status === "Chưa được phép bán" ? "bg-red" : "bg-green-500"}`}>
-                        {product.status}
+                  <td className="px-4 py-5">{product.id}</td>
+                  <td className="px-4 py-5">{product.name}</td>
+                  <td className="px-4 py-5">
+                    <div
+                      className={`px-2 py-1 w-fit rounded-md text-white text-[12px] ${product.status === "Chưa được phép bán" ? "bg-red" : "bg-green-500"}`}
+                    >
+                      {product.status}
                     </div>
                   </td>
-                  <td className="p-4">{product.stock}</td>
-                  <td className="p-4">{product.createdAt}</td>
+                  <td className="px-4 py-5">{product.stock}</td>
+                  <td className="px-4 py-5">{product.createdAt}</td>
                 </tr>
               ))}
             </tbody>
@@ -124,43 +226,12 @@ const Products: React.FC = () => {
         </div>
 
         {/* Pagination */}
-        <div className="flex justify-end items-center gap-2 mt-6 text-[13px]">
-          <button
-            className="px-3 py-1 rounded-lg disabled:opacity-50"
-            onClick={() => handleChangePage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            <ChevronLeft
-              size={24}
-              className="text-grey-900 hover:text-blue-500"
-            />
-          </button>
-
-          {visiblePages.map((page) => (
-            <button
-              key={page}
-              className={`w-8 h-8 flex items-center justify-center rounded-lg cursor-pointer ${
-                currentPage === page
-                  ? "bg-primary-gradient text-white"
-                  : "hover:text-blue-500"
-              }`}
-              onClick={() => handleChangePage(page)}
-            >
-              {page}
-            </button>
-          ))}
-
-          <button
-            className="px-3 py-1 disabled:opacity-50"
-            onClick={() => handleChangePage(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            <ChevronRight
-              size={24}
-              className="text-grey-900 hover:text-blue-500"
-            />
-          </button>
-        </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          visiblePages={visiblePages}
+          onChangePage={setCurrentPage}
+        />
       </div>
     </AdminLayout>
   );
